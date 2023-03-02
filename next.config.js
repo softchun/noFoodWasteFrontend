83ab8c244018e78096f4dbe2d3af33710a35879a
Your Name <soft.chunya@gmail.com>
2023-03-02 16:46:04 +0700

    delete comment

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['firebasestorage.googleapis.com'],
-    // remotePatterns: [
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'firebasestorage.googleapis.com',
-    //     port: '',
-    //     pathname: '/v0/b/nofoodwaste-project.appspot.com/**',
-    //   },
-    // ],
   },
 }
 
