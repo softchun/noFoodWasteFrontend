3c6ad9702a56c3cbfd458bfb98fd1c58858e556f
Your Name <soft.chunya@gmail.com>
2023-03-02 16:52:38 +0700

    add disabled

--- src/components/ui/toggle.tsx
+++ src/components/ui/toggle.tsx
@@ -3,15 +3,16 @@ import { useState } from "react";
 type Props = {
     title?: string,
     enabled: boolean,
-    setEnabled: any
+    setEnabled: any,
+    disabled?: boolean,
 }
-export default function Toggle({title, enabled, setEnabled}: Props) {
+export default function Toggle({title, enabled, setEnabled, disabled=false}: Props) {
     // const [enabled, setEnabled] = useState(false);
 
     return (
-        <div className="relative flex flex-col items-center justify-center overflow-hidden">
+        <div className={`relative flex flex-col items-center justify-center overflow-hidden  ${disabled && 'text-gray-4'}`}>
             <div className="flex">
-                <label className="inline-flex relative items-center mr-5 cursor-pointer">
+                <label className="inline-flex relative items-center cursor-pointer">
                     <input
                         type="checkbox"
                         className="sr-only peer"
@@ -19,8 +20,8 @@ export default function Toggle({title, enabled, setEnabled}: Props) {
                         readOnly
                     />
                     <div
-                        onClick={() => setEnabled(!enabled)}
-                        className="w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-primary  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-primary"
+                        onClick={() => {if(!disabled) setEnabled(!enabled);}}
+                        className={`w-11 h-6 bg-gray-200 rounded-full peer  ${disabled?'peer-focus:ring-gray-4':'peer-focus:ring-primary'} peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all ${disabled?'peer-checked:bg-gray-4':'peer-checked:bg-primary'}`}
                     ></div>
                     {title && <span className={`ml-2 ${enabled? 'font-medium':'font-normal'}`}>
                         {title}
