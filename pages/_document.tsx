e7e81d172fceacc1bb54ff1a821096bae4c68a55
Your Name <soft.chunya@gmail.com>
2023-03-02 16:46:42 +0700

    add font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,9 @@ export default function Document() {
         <Html>
             <Head>
                 {/* <link href='https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.3/leaflet.css' rel='stylesheet'></link> */}
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Kodchasan&display=swap" rel="stylesheet" />
             </Head>
             <body>
                 <Main />
