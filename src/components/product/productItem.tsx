6d11dcfa4301b9a99de5abc511725e6cd0db66fd
Your Name <soft.chunya@gmail.com>
2023-03-07 00:52:47 +0700

    fix onClick

--- src/components/product/productItem.tsx
+++ src/components/product/productItem.tsx
@@ -27,13 +27,14 @@ type ItemData = {
 type Props = {
     data?: ItemData,
     style?: string,
+    onClickButton?: any,
 }
 
-function ProductItem({data, style}: Props) {
+function ProductItem({data, style, onClickButton}: Props) {
     // console.log(data)
 
     return (
-        <div className={`flex gap-4 w-[440px] rounded-3xl p-4 relative bg-white ${style}`}>
+        <div className={`flex gap-4 w-[440px] rounded-3xl p-4 relative bg-white ${style}`} onClick={() => onClickButton? onClickButton():null}>
             <div className='bg-gray-4 rounded-3xl min-w-[110px] max-w-[110px] h-[110px] overflow-hidden relative'>
                 {data.image  &&
                     <Image src={data.image} alt='image' layout="fill" objectFit="cover" />
